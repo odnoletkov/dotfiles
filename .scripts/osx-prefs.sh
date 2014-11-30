5234755e6c00c1367be5e295ec4c3f666ea097ae
Maksim Odnoletkov <odnoletkov@mail.ru>
2014-11-30 19:20:08 +0300

    adjusted key repeat settings

--- .scripts/osx-prefs.sh
+++ .scripts/osx-prefs.sh
@@ -85,7 +85,7 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 echo ""
 echo "Setting a blazingly fast keyboard repeat rate"
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 echo ""
