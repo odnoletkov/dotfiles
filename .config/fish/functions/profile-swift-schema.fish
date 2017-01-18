c67dfe897523406173ab15448dbafc7ac5114026
Maksim Odnoletkov <odnoletkov@mail.ru>
2017-01-18 20:58:37 +0300

    profile-swift-schema in bash

--- .config/fish/functions/profile-swift-schema.fish
+++
@@ -1,3 +0,0 @@
-function profile-swift-schema
-  xcodebuild -scheme $argv[1] -sdk iphonesimulator clean build OTHER_SWIFT_FLAGS="-Xfrontend -debug-time-function-bodies" | grep -E "^[0-9]+.[0-9]+ms" | sort -nr | head -n 20
-end
